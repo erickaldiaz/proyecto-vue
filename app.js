cb6029a7930636a0a981892c636baa13d93b80eb
Ericka Diaz <erickaldiaz@gmail.com>
2021-03-27 00:32:23 -0300

    add styles in real time

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ new Vue ({
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: -10,
+            color: 'f4f4f4',
 
             price: 8400,
             pricesWithDays: [
@@ -24,6 +25,8 @@ new Vue ({
     methods: {
         toggleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.color = this.color.split('').reverse().join('')
         }
     }
 })
